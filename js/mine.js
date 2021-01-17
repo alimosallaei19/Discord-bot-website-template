c32f860c248a566750cdf712a8ff874838af28ab
Ali Mosallaei <22mosallaei@lexingtonma.org>
2021-01-17 04:42:00 +0000

    nice

---
+++ js/mine.js
@@ -0,0 +1,3 @@
+function goToLogin(){
+  window.location.href = `https://discord.com/api/oauth2/authorize?client_id=784927010819801109&redirect_uri=https%3A%2F%2Fdiamond-nebulous-jobaria.glitch.me&response_type=code&scope=identify`
+}
